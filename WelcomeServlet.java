d648df223fae6229fa5e7d9ac5a2dbdcb8f82163
Hrushiprada Swain <hrushipradaswain@gmail.com>
2026-02-02 23:47:22 +0530

    01First_Servlet_WebApp
    To start my first web application using servlet-api

---
+++ WelcomeServlet.java
@@ -0,0 +1,22 @@
+package in.hrushiit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h1>Welcome to my Servlet Project</h1>");
+    }
+}
+
