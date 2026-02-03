78a7676d999ffda0c3f3172d2069c02a7509d9c5
Hrushiprada Swain <hrushipradaswain@gmail.com>
2026-02-04 01:12:11 +0530

    SecondServlet_Project

---
+++ FirstServlet.java
@@ -0,0 +1,22 @@
+package in.hrushiit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request , HttpServletResponse response)throws IOException {
+	
+		PrintWriter pw =response.getWriter();
+		
+		pw.append("<h1>Welcome to Web Development</h1>");
+		
+	}
+
+}
